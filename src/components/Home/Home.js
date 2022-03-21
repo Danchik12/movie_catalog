e472ca8b72275104f73a51d99b9cbcb3d632e277
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-03-21 11:03:07 +0300

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,13 @@
 import {Main} from './../elements/Main/Main'
 import s from './Home.module.scss'
 import {NavigateBar} from './../UI/Navigation/NavigateBar'
+
+
+
 export const Home =() =>{
+	
 	return (
-		<div className='d-flex'>
+		<div className={s.home}>
 		<NavigateBar / >
 		<Main/>
 

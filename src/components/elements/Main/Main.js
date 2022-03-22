33f124b992414811772fe4c67207736af2d02449
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-03-22 09:03:44 +0300

    upload Main

--- src/components/elements/Main/Main.js
+++ src/components/elements/Main/Main.js
@@ -6,7 +6,21 @@ export const Main = () => {
 
 return (
 	<div className={s.container}>
-	
+<div className={s.new}>Новинки
+<i className='bx bxs-left-arrow'></i>
+<i className='bx bxs-right-arrow'></i>
+
+
+</div>
+
+
+<div className={s.serials}>
+Сериалы
+<i className='bx bxs-left-arrow'></i>
+<i className='bx bxs-right-arrow'></i>
+
+
+</div>
 	</div>
 	)
 

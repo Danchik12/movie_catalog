b49186e802df64d2b9ca0574fa204eb4b969a6cf
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-03-22 09:03:12 +0300

    add Bookmarks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Header from './components/elements/Header/Header'
 import {Routes,Route} from 'react-router-dom'
 import {Home} from './components/Home/Home'
+import {Bookmarks} from './components/Bookmarks/Bookmarks'
 function App() {
   return (
     <div className="App">
     <Header/>
     <Routes>
     <Route path='/' exact element={<Home/>} />
-    
+    <Route path='/bookmarks' exact element={<Bookmarks/>}/>
     </Routes>
     </div>
   );
---
+++ src/components/Bookmarks/Bookmarks.js
@@ -0,0 +1,18 @@
+
+import s from './Bookmarks.module.scss'
+import {NavigateBar} from './../UI/Navigation/NavigateBar'
+
+
+
+export const Bookmarks =() =>{
+	
+	return (
+		<div className={s.bookmark}>
+		<NavigateBar / >
+
+
+
+		</div>
+
+		)
+}

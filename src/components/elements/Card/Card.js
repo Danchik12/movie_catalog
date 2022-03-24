b7c610665e55aeb4518db2245c9f90add8651271
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-03-24 12:33:40 +0300

    carousel upgrade

--- src/components/elements/Card/Card.js
+++ src/components/elements/Card/Card.js
@@ -5,7 +5,10 @@ export const Card = ({movie}) => {
 
 
 return (
-	<div className={s.card}></div>
+	<div className={s.card}>
+<img src={movie.poster}  alt={movie.title} className={s.poster}/>
+<p>{movie.title} ({movie.year})</p>
+	</div>
 
 	)
 
--- src/components/elements/Carousel/Carousel.js
+++ src/components/elements/Carousel/Carousel.js
@@ -2,27 +2,45 @@ import s from  './Carousel.module.scss'
 import {Card} from './../Card/Card'
 
 export const Carousel = () => {
-
+var data=[
+{poster:'https://upload.wikimedia.org/wikipedia/ru/1/13/Batman_poster.jpg',
+title:'Batman',
+year:'2019'},
+{poster:'https://m.media-amazon.com/images/M/MV5BNzRmZWJhNjUtY2ZkYy00N2MyLWJmNTktOTAwY2VkODVmOGY3XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg',
+title:'Supernatural',
+year:'2005'},
+{poster:'https://www.kino-teatr.ru/movie/posters/big/3/35933.jpg',
+title:'Merlin',
+year:'2004'},
+{poster:'https://www.film.ru/sites/default/files/movies/posters/superman_returns_ver8_xlg.jpg',
+title:'Superman',
+year:'2010'},
+{poster:'https://cdn.europosters.eu/image/1300/posters/doctor-who-day-of-the-doctor-i15143.jpg',
+title:'Doctor Who',
+year:'2002'}
+]
 
 return (
 	<div className={s.container}>
 <div className={s.new}>Новинки
-<div className={s.controls}>
-<span className={s.control} ><i className='bx bxs-left-arrow' ></i></span>
-<span className={s.control} ><i className='bx bxs-right-arrow'  ></i></span>
-</div>
+<ul className={s.carousel}>
+{data.map((date,index) => 
+<Card movie={date} key={index}  />
+
+)}
+
+</ul>
 </div>
 
 
 <div className={s.serials}>
 Сериалы
-<div className={s.controls} >
-<span className={s.control} ><i className='bx bxs-left-arrow' ></i></span>
+<ul className={s.carousel}>
+{data.map((date,index) => 
+<Card movie={date} key={index}  />
 
-<span className={s.control} ><i className='bx bxs-right-arrow'  ></i></span>
-</div>
-
-
+)}
+</ul>
 </div>
 </div>
 	)

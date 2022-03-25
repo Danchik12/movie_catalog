7d9804e84a1410977e8a298734dff6a74eeaa71b
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-03-25 09:19:31 +0300

    update card

--- src/components/elements/Card/Card.js
+++ src/components/elements/Card/Card.js
@@ -6,7 +6,11 @@ export const Card = ({movie}) => {
 
 return (
 	<div className={s.card}>
-<img src={movie.poster}  alt={movie.title} className={s.poster}/>
+	<span className={s.bookmark}><i className='bx bx-bookmark'></i></span>
+	<img src={movie.poster}  alt={movie.title} className={s.poster}/>
+	
+	
+	
 <p>{movie.title} ({movie.year})</p>
 	</div>
 

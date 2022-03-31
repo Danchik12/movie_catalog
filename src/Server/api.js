ba3318d216f0ce12bca5a6002336dd0cebc6affb
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-03-31 10:01:44 +0300

    redux install

--- src/Server/api.js
+++ src/Server/api.js
@@ -1,1 +1,17 @@
+import axios from 'axios'
 
+const api =axios.create({
+baseURL:process.env.REACT_APP_API_URL,
+
+
+
+}
+)
+
+api.interceptors.request.use(config =>{
+	config.url =config.url +  '?api_key='+process.env.REACT_APP_API_KEY+'&language=ru-RU'
+	return config
+})
+
+
+export default api

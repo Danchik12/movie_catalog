5dd623ffb1d34e65f555ecd387979fc6183092a0
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-03-31 15:18:05 +0300

    Update Main.js

--- src/components/elements/Main/Main.js
+++ src/components/elements/Main/Main.js
@@ -1,56 +1,31 @@
 import s from  './Main.module.scss'
 import {Card} from './../Card/Card'
-
+import React,{useEffect} from 'react'
+import Loader from './../../UI/Loader/Loader'
+import { getTrendsMovie,getTrendsSerial} from './../../../store/thunk/servis'
+import {useDispatch,useSelector} from 'react-redux'
 export const Main = () => {
-var data=[
-{poster:'https://upload.wikimedia.org/wikipedia/ru/1/13/Batman_poster.jpg',
-title:'Бэтмен',
-year:'2019',
-id:'1'},
-{poster:'https://m.media-amazon.com/images/M/MV5BNzRmZWJhNjUtY2ZkYy00N2MyLWJmNTktOTAwY2VkODVmOGY3XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg',
-title:'Сверхъестественное',
-year:'2005',
-id:'2'},
-{poster:'https://avatars.mds.yandex.net/get-kinopoisk-image/1773646/ea31bf58-de0e-41ac-8179-e60a94e3f24c/600x900',
-title:'Мерлин',
-year:'2004',
-id:'3'},
-{poster:'https://www.film.ru/sites/default/files/movies/posters/superman_returns_ver8_xlg.jpg',
-title:'Супермэн',
-year:'2010',
-id:'4'},
-{poster:'https://cdn.europosters.eu/image/1300/posters/doctor-who-day-of-the-doctor-i15143.jpg',
-title:'Доктор Кто: День доктора',
-year:'2002',
-id:'5'},
-{
-poster:'https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/731c4031-7389-44f4-8c15-f9f4e3b0ed90/300x450',
-title:'Человек паук : Нет пути домой',
-year:'2021',
-id:'6'
-},
-{
-poster:'https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/5cadd28b-996e-4224-ab98-f7aed64aa56c/600x900',
-title:'Человек паук : Вдали от дома',
-year:'2019',
-id:'7'
-},
-{
-poster:'https://upload.wikimedia.org/wikipedia/ru/5/5a/Spider-Man_Homecoming_logo.jpg',
-title:'Человек паук : Возвращение домой',
-year:'2017',
-id:'8'
-}
-
-]
+const dispatch =useDispatch()
+	const {movies,isLoading ,serials} = useSelector((state) => state.movie)
+	
+//useEffect(()=>{
+//	dispatch(getTrendsMovie())
+//	dispatch(getTrendsSerial())
+//
+//},[])
 
 return (
+
 	<div className={s.container}>
-<div className={s.new}>Новинки
+{ isLoading  ?
+<Loader/>
+	:
+	<>
+<div className={s.new}>Популярные фильмы
 
 <ul className={s.list}>
-{data.map((date,index) => 
-<Card movie={date} key={index}  />
+{movies.results.map((data,index) => 
+<Card movie={data} key={index}  />
 
 )}
 
@@ -60,15 +35,19 @@ return (
 
 
 <div className={s.serials}>
-Сериалы
+Популярные сериалы
 <ul className={s.list}>
-{data.map((date,index) => 
-<Card movie={date} key={index}  />
+{serials.results.map((data,index) => 
+<Card movie={data} key={index}  />
 
 )}
 </ul>
 </div>
+</>
+}
 </div>
+
+
 	)
 
 }

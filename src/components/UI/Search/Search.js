f52469c7ed60b930f4091510c0667abc79545b46
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-04-03 15:02:24 +0300

    add Search

--- src/components/UI/Search/Search.js
+++ src/components/UI/Search/Search.js
@@ -1,13 +1,15 @@
 import s from './Search.module.scss'
-
-
+import {useDispatch,useSelector} from 'react-redux'
+import {getSearch} from './../../../store/thunk/servis'
 
 export const Search = () => {
+	const dispatch =useDispatch()
+		const {isLoading ,search} = useSelector((state) => state.movie)
 	return (
 		<div className={s.search}>
 		
 		
-		<input type='text' placeholder="Поиск " />
+		<input type='text' placeholder="Введите название " onKeyPress={e => e.key === 'Enter'  && dispatch(getSearch(e.target.value)) } />
 		<i className='bx bx-search-alt'></i>
 		
 		
--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import s from './Header.module.scss'
 import {Search} from './../../UI/Search/Search'
 
@@ -15,7 +14,7 @@ return(
 SMART<span style={{'color':'#81ae65'}}>Movies</span>
 </a>
 
-<Search/>
+<Search />
 
 
 

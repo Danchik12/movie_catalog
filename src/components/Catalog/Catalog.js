905c9237b03d8dd247592aaf49bd54e6690c446c
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-04-07 20:12:59 +0300

    Search Update

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -3,35 +3,37 @@ import {Card} from './../elements/Card/Card'
 import s from './Catalog.module.scss'
 import Loader from './../UI/Loader/Loader'
 import {useDispatch,useSelector} from 'react-redux'
-
+import {useLocation} from 'react-router-dom'
 
 
 
 export default function Catalog(){
-const dispatch =useDispatch()
+	const dispatch =useDispatch()
 const {movies,isLoading ,serials,search} = useSelector((state) => state.movie)
-  const items=[
-  	{media_type:'film',
-  		id:666,
-  		title:'SpongeBob',
-  		poster_path:333},
-  		{media_type:'tv',
-  		id:7777,
-  		title:'Supernatural',
-  		poster_path:666},
-  		{media_type:'film',
-  		id:328,
-  		title:'Atack',
-  		poster_path:999}
+	const location =useLocation()
+	
+//	if (location.pathname == '/films'){
+//		useEffect(() => {
+//				dispatch()
+//			},[])
+//	}
+	//	else if (location.pathname == '/serials'){
+	//		useEffect(() => {
+	//			dispatch()
+	//		},[])
+//		}
+		//else {
+			const items=search
+			
+		//}
 
-  ]
-
+ 
 return(
 	<div className={s.Catalog}>
 	{
-		isLoading
+		isLoading 
 		? <Loader/>
-		:items.map(item => <Card movie={item} key={item.id}/>)
+		:items.results.map(item => <Card movie={item} key={item.id}/>)
 
 		
 
--- src/components/UI/Search/Search.js
+++ src/components/UI/Search/Search.js
@@ -1,15 +1,36 @@
 import s from './Search.module.scss'
 import {useDispatch} from 'react-redux'
+import React,{useEffect} from 'react'
+import {useNavigate,createSearchParams,useSearchParams} from 'react-router-dom'
 import {getSearch} from './../../../store/thunk/servis'
 
 export const Search = () => {
+	const [searchParams] = useSearchParams()
+	
 	const dispatch =useDispatch()
+	let navigate=useNavigate()	
+	useEffect(() => {
+    const currentParams = Object.fromEntries([...searchParams]);
+   
+    dispatch(getSearch(currentParams.query)) 
+  }, [searchParams]);
+
+	function Press(e){
+		dispatch(getSearch(e.target.value))
 		
+		navigate({
+      pathname: '/search',
+      search: `?${createSearchParams({query:e.target.value})}`,
+    })
+		  
+		 
+
+		}
 	return (
 		<div className={s.search}>
 		
 		
-		<input type='text' placeholder="Введите название " onKeyPress={e => e.key === 'Enter'  && dispatch(getSearch(e.target.value)) } />
+		<input type='text' placeholder="Введите название " onKeyPress={e => e.key === 'Enter'  && Press(e) } />
 		<i className='bx bx-search-alt'></i>
 		
 		

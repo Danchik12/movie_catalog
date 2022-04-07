95e74898c1f15fbf3ee6d7e22dded855748db029
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-04-07 12:35:12 +0300

    update Card

--- src/components/elements/Card/Card.js
+++ src/components/elements/Card/Card.js
@@ -4,18 +4,19 @@ import s from './Card.module.scss'
 export const Card = ({movie}) => {
 
 return (
-	
+	<a href={'/info/'+movie.media_type+'/'+movie.id}   >
 	<div className={s.card}>
 	
-	<a href={'/info/'+movie.media_type+'/'+movie.id}   >
+	
 	<img src= {'https://image.tmdb.org/t/p/original/'+movie.poster_path} alt={movie.title ? movie.title : movie.name} className={s.poster}/>
 	
 	
 	
 <p>{movie.title ? movie.title : movie.name}</p>
-</a>
+
 
 	</div>
+	</a>
 	
 
 	)

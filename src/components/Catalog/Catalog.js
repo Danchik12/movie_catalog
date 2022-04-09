17c7214a7be155bde08fd9cf6099a16f740fd3a8
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-04-09 20:59:01 +0300

    Update Catalog.js

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -3,19 +3,13 @@ import {Card} from './../elements/Card/Card'
 import s from './Catalog.module.scss'
 import Loader from './../UI/Loader/Loader'
 import {useSelector} from 'react-redux'
-
+import {useLocation} from 'react-router-dom'
 
 
 export default function Catalog({items}){
-
+const location =useLocation()
 
 const {isLoading } = useSelector((state) => state.movie)
-
-
-	
-
-	
-
 
 
  
@@ -24,7 +18,11 @@ return(
 	{
 		isLoading 
 		? <Loader/>
-		: items.results.map(item => <Card movie={item} key={item.id}/>)
+
+		: 
+
+
+items.results.map(item => <Card movie={Object.assign({media_type:location.pathname == '/films' ? 'movie' : 'tv'},item)} key={item.id}/>)
 
 		
 
